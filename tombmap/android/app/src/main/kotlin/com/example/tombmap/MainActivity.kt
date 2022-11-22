df0f5db235c66617dfcd024eaf2466152b69b2a2
Lucas  Honorato da Cruz <81528223+honorato-cmd@users.noreply.github.com>
2022-11-22 20:19:16 -0300

    Deletar

--- tombmap/android/app/src/main/kotlin/com/example/tombmap/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tombmap
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- tombmap/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'dart:io';
-
-import 'views/ios/ios.app.dart';
-import 'views/android/android.app.dart';
-
-void main() {
-  runApp(Platform.isIOS ? IosApp() : AndroidApp());
-}
